e798571e7d562c3a9d6634ceb5c9d1673e78bec6
Kishan Patel <kishanpatel789@gmail.com>
2025-05-21 13:39:55 -0500

    Complete test coverage for in-memory

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -5,11 +5,15 @@ from src.snipster.models import LangEnum, Snippet, Tag
 from src.snipster.repo import InMemorySnippetRepository
 
 
-@pytest.fixture()
-def example_snippet() -> Snippet:
+@pytest.fixture(scope="function")
+def repo() -> InMemorySnippetRepository:
+    return InMemorySnippetRepository()
+
+
+@pytest.fixture(scope="function")
+def add_snippet(repo) -> Snippet:
     tag_beginner = Tag(name="beginner")
     tag_training = Tag(name="training")
-
     snippet = Snippet(
         title="First snip",
         code="print('hello world')",
@@ -17,29 +21,47 @@ def example_snippet() -> Snippet:
         language=LangEnum.PYTHON,
         tags=[tag_beginner, tag_training],
     )
-
+    repo.add(snippet)
     return snippet
 
 
-def test_full_in_mem_implementation(example_snippet):
-    repo = InMemorySnippetRepository()
-    repo.add(example_snippet)
+@pytest.fixture(scope="function")
+def add_another_snippet(repo) -> Snippet:
+    snippet = Snippet(
+        title="Get it all",
+        code="SELECT * FROM MY_TABLE;",
+        description="Get all records from MY_TABLE",
+        language=LangEnum.SQL,
+    )
+    repo.add(snippet)
+    return snippet
+
+
+def test_add_snippet(repo, add_snippet):
+    assert repo._snippets[1] == add_snippet
+
+
+def test_list_snippets_one_snippet(repo, add_snippet):
     assert len(repo.list()) == 1
 
-    retrieved = repo.get(1)
-    assert retrieved.id == 1
-    assert retrieved.title == "First snip"
-    assert retrieved.code == "print('hello world')"
-    assert retrieved.description == "Say hello snipster"
-    assert retrieved.language == LangEnum.PYTHON
-    assert len(retrieved.tags) == 2
-    assert retrieved.created_at is not None
-    assert retrieved.updated_at is None
-    assert retrieved.favorite is False
 
-    with pytest.raises(SnippetNotFoundError):
-        repo.delete(2)
+def test_list_two_snippets_two_snippets(repo, add_snippet, add_another_snippet):
+    assert len(repo.list()) == 2
 
+
+def test_get_snippet(repo, add_snippet):
+    assert repo.get(1) == add_snippet
+
+
+def test_get_snippet_not_found(repo, add_snippet):
+    assert repo.get(99) is None
+
+
+def test_delete_snippet(repo, add_snippet):
     repo.delete(1)
     assert len(repo.list()) == 0
-    assert repo.get(1) is None
+
+
+def test_delete_snippet_not_found(repo):
+    with pytest.raises(SnippetNotFoundError):
+        repo.delete(99)
