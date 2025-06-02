7fbd6a6d2d509f598fbe955973d79df8aea8d9fe
Kishan Patel <kishanpatel789@gmail.com>
2025-06-02 14:06:12 -0500

    Add basic crud features to cli

--- src/snipster/cli.py
+++ src/snipster/cli.py
@@ -1,12 +1,69 @@
+import typer
+from sqlmodel import create_engine
 from typer import Typer
+
+from .models import LangEnum, Snippet
+from .repo import DBSnippetRepository
 
 app = Typer()
 
 
+@app.callback()
+def init(ctx: typer.Context):
+    engine = create_engine("sqlite:///snipster.sqlite", echo=False)
+    ctx.obj = DBSnippetRepository(engine)
+
+
 @app.command()
-def main(name: str):
-    print(f"Hello {name}")
+def add(title: str, code: str, ctx: typer.Context):
+    repo: DBSnippetRepository = ctx.obj
+    snippet = Snippet.create(
+        title=title,
+        code=code,
+        description="A new snippet",
+        language=LangEnum.PYTHON,
+    )
+    repo.add(snippet)
 
 
-if __name__ == "__main__":
-    app()
+@app.command()
+def list(ctx: typer.Context):
+    repo: DBSnippetRepository = ctx.obj
+    snippets = repo.list()
+    if snippets:
+        print(snippets)
+    else:
+        print("No snippets found.")
+
+
+@app.command()
+def get(snippet_id: int, ctx: typer.Context):
+    repo: DBSnippetRepository = ctx.obj
+    snippet = repo.get(snippet_id)
+    if snippet:
+        # print(f"Snippet ID: {snippet.id}, Title: {snippet.title}, Code: {snippet.code}")
+        print(snippet)
+    else:
+        print(f"No snippet found with ID {snippet_id}")
+
+
+@app.command()
+def delete(snippet_id: int, ctx: typer.Context):
+    repo: DBSnippetRepository = ctx.obj
+    repo.delete(snippet_id)
+    print(f"Snippet {snippet_id} is deleted.")
+
+
+@app.command()
+def search(ctx: typer.Context):
+    pass
+
+
+@app.command()
+def toggle_favorite(ctx: typer.Context):
+    pass
+
+
+@app.command()
+def tag(ctx: typer.Context):
+    pass
